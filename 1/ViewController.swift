f722b3c3f6405d0dfffe8c34fb1f4d904bcc9951
Nuraiym <1991958945@qq.com>
2022-11-21 18:11:43 +0600

    saa

--- 1/ViewController.swift
+++ 1/ViewController.swift
@@ -9,6 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var lastLabel: UILabel!
+    
+    var userName = ""
+    
+    var password = ""
+    
+    
+    @IBAction func click(_ sender: UIButton) {
+        userName = userNameTextField.text!
+        password = passwordTextField.text!
+        
+        
+        if userName.count >= 3 {
+        if password.count >= 3 {
+            lastLabel.textColor = .green
+            lastLabel.text = "Успешно"
+        }else{
+            lastLabel.textColor = .red
+            lastLabel.text = "Не верно"
+        }
+    }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
